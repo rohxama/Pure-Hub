f9f20cd997494d8494d510b059b50a7f1b348724
rohxama <rohxama@gmail.com>
2026-07-04 00:12:48 +0500

    remove Footer import from Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,7 +1,6 @@
 import { useLocation } from 'react-router-dom'
 import { useEffect } from 'react'
 import Navbar from './Navbar'
-import Footer from './Footer'
 
 export default function Layout({ children }) {
   const { pathname } = useLocation()
@@ -14,7 +13,6 @@ export default function Layout({ children }) {
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <main className="flex-1 pt-16 lg:pt-20">{children}</main>
-      <Footer />
     </div>
   )
 }
