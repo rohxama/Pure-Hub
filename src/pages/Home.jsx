22651df66e3a7387f8a7cce5cceca806b5c8de83
rohxama <rohxama@gmail.com>
2026-07-04 00:13:11 +0500

    keep only Hero and Features sections on Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,11 @@
 import Hero from '@/components/sections/Hero'
-import Categories from '@/components/sections/Categories'
-import FeaturedProducts from '@/components/sections/FeaturedProducts'
 import Features from '@/components/sections/Features'
-import Testimonials from '@/components/sections/Testimonials'
 
 export default function Home() {
   return (
     <>
       <Hero />
-      <Categories />
-      <FeaturedProducts />
       <Features />
-      <Testimonials />
     </>
   )
 }
