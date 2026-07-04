338a9bf559d3dd077e0af002dcefbefe52bf4858
rohxama <rohxama@gmail.com>
2026-07-04 11:00:26 +0500

    add About section after Features on Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from '@/components/sections/Hero'
 import Features from '@/components/sections/Features'
+import AboutSection from '@/components/sections/AboutSection'
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <AboutSection />
     </>
   )
 }
