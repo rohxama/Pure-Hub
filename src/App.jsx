c182edd34d6eb0e167d846d699db0b770e5baf3d
rohxama <rohxama@gmail.com>
2026-07-05 23:25:55 +0500

    add /products route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '@/components/layout/Layout'
 import Home from '@/pages/Home'
+import Products from '@/pages/Products'
 import ProductDetail from '@/pages/ProductDetail'
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
         <Route path="/product/:slug" element={<ProductDetail />} />
       </Routes>
     </Layout>
