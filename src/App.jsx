1b74c7e2e6d62a684f89ad54a74ef98e262bfcee
rohxama <rohxama@gmail.com>
2026-07-05 23:22:22 +0500

    add route for /product/:slug

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import Layout from '@/components/layout/Layout'
 import Home from '@/pages/Home'
+import ProductDetail from '@/pages/ProductDetail'
 
 export default function App() {
   return (
     <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product/:slug" element={<ProductDetail />} />
       </Routes>
     </Layout>
   )
