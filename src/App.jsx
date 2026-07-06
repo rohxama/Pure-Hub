32c77658c29c2cf999d4a386947aca3c5d03c24a
rohxama <rohxama@gmail.com>
2026-07-06 17:11:50 +0500

    add routes for About, Blog, Contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Layout from '@/components/layout/Layout'
 import Home from '@/pages/Home'
 import Products from '@/pages/Products'
 import ProductDetail from '@/pages/ProductDetail'
+import About from '@/pages/About'
+import Blog from '@/pages/Blog'
+import Contact from '@/pages/Contact'
 
 export default function App() {
   return (
@@ -11,6 +14,9 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/product/:slug" element={<ProductDetail />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </Layout>
   )
